b03c99e42b1ce66a87d9ed1a9e5663dd11606439
SrbinUSvemiru <91745471+SrbinUSvemiru@users.noreply.github.com>
2022-04-24 14:32:40 +0200

    temp

--- src/components/TileThirdTemp.js
+++ src/components/TileThirdTemp.js
@@ -1,20 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SectionTwo.css";
 
 function TileThirdTemp({ hour, units }) {
-  const height = `${hour.temp * 3}px`;
-  const heightFarenheit = `${(((hour.temp - 32) * 5) / 9) * 3}px`;
+  const [newUnits, setNewUnits] = useState();
+
+  useEffect(() => {
+    const height = `${hour.temp * 3}px`;
+    setNewUnits(height);
+  }, []);
 
   return (
     <>
-      {units === "metric" ? (
-        <div className="tile-third-temp" style={{ height: height }}></div>
-      ) : (
-        <div
-          className="tile-third-temp"
-          style={{ height: heightFarenheit }}
-        ></div>
-      )}
+      <div className="tile-third-temp" style={{ height: newUnits }}></div>
     </>
   );
 }
